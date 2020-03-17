81e19530ec6a811e35acb01e47ef63d59abc988b
julianodesenv <julianodesenv@gmail.com>
2020-03-17 16:55:21 -0300

    configuration passport

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //client_id=4&redirect_uri=http%3A%2F%2Flocalhost%2Fcallback&response_type=code&scope=
+    $query = http_build_query([
+        'client_id' => 4,
+        'redirect_uri' => 'http://localhost/callback',
+        'response_type' => 'code',
+        'scope' => '',
+    ]);
+
+    return redirect("http://localhost:8000/oauth/authorize?$query");
 });
+
+Route::get('/callback', function (\Illuminate\Http\Request $request) {
+    $code = $request->get('code');
+    $http = new \GuzzleHttp\Client();
+    $response = $http->post('http://localhost:8000/oauth/token', [
+        'form_params' => [
+            'client_id' => 4,
+            'client_secret' => 'KZ9YczpnSzZLTgNDnQLCxAHW8izLbxcEP9oSCaS8',
+            'redirect_uri' => 'http://localhost/callback',
+            'code' => $code,
+            'grant_type' => 'authorization_code'
+        ]
+    ]);
+    dd(json_decode($response->getBody(), true));
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
